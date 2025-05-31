8529a2d77fcada61aff7dea8f9810be1c407f9c8
DESKTOP-JMR591K\Administrator <jaon997@gmail.com>
2025-05-31 17:02:33 +0200

    remove node dependency when building

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SPLIIT_APP_NAME=$(node -p -e "require('./package.json').name")
-SPLIIT_VERSION=$(node -p -e "require('./package.json').version")
+SPLIIT_APP_NAME=$(sed -n 's/.*"name": *"\([^"]*\)".*/\1/p' package.json)
+SPLIIT_VERSION=$(sed -n 's/.*"version": *"\([^"]*\)".*/\1/p' package.json)
 
 # we need to set dummy data for POSTGRES env vars in order for build not to fail
 docker buildx build \
