2950476b9d1c280a59d09df940f56ae0ceb102a1
DESKTOP-JMR591K\Administrator <jaon997@gmail.com>
2026-02-11 00:20:52 +0100

    build and dev fixes

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -4,9 +4,6 @@ SPLIIT_APP_NAME=$(sed -n 's/.*"name": *"\([^"]*\)".*/\1/p' package.json)
 SPLIIT_VERSION=$(sed -n 's/.*"version": *"\([^"]*\)".*/\1/p' package.json)
 
 # we need to set dummy data for POSTGRES env vars in order for build not to fail
-docker buildx build \
-    -t ${SPLIIT_APP_NAME}:${SPLIIT_VERSION} \
-    -t ${SPLIIT_APP_NAME}:latest \
-    .
+docker build -t "${SPLIIT_APP_NAME}:${SPLIIT_VERSION}" -t "${SPLIIT_APP_NAME}:latest" .
 
 #docker image prune -f
